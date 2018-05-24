a959723db370c316ae0d1eee255ff818c3a5243e
Jonathan Stegall <jstegall@minnpost.com>
2018-05-24 12:56:59 -0500

    update node stuff, and fix a javascript error

--- assets/js/src/admin/sort.js
+++ assets/js/src/admin/sort.js
@@ -1,10 +1,12 @@
-jQuery(document).ready(function($){
-	$('.m-staff-list').sortable({
-		axis: 'y',
-		cursor: 'move',
-		opacity: 0.6,
-		cursor: 'move',
-		distance: 5,
-		update: function() {}
-	});
+jQuery( document ).ready( function( $ ) {
+	if ( $( '.m-staff-list' ).length > 0 ) {
+		$( '.m-staff-list' ).sortable({
+			axis: 'y',
+			cursor: 'move',
+			opacity: 0.6,
+			cursor: 'move',
+			distance: 5,
+			update: function() {}
+		});
+	}
 });
--- assets/js/staff-user-post-list-admin.js
+++ assets/js/staff-user-post-list-admin.js
@@ -3,12 +3,14 @@
 function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
 
 jQuery(document).ready(function ($) {
-	var _$$sortable;
+	if ($('.m-staff-list').length > 0) {
+		var _$$sortable;
 
-	$('.m-staff-list').sortable((_$$sortable = {
-		axis: 'y',
-		cursor: 'move',
-		opacity: 0.6
-	}, _defineProperty(_$$sortable, 'cursor', 'move'), _defineProperty(_$$sortable, 'distance', 5), _defineProperty(_$$sortable, 'update', function update() {}), _$$sortable));
+		$('.m-staff-list').sortable((_$$sortable = {
+			axis: 'y',
+			cursor: 'move',
+			opacity: 0.6
+		}, _defineProperty(_$$sortable, 'cursor', 'move'), _defineProperty(_$$sortable, 'distance', 5), _defineProperty(_$$sortable, 'update', function update() {}), _$$sortable));
+	}
 });
-//# sourceMappingURL=data:application/json;charset=utf8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbInNvcnQuanMiXSwibmFtZXMiOlsialF1ZXJ5IiwiZG9jdW1lbnQiLCJyZWFkeSIsIiQiLCJzb3J0YWJsZSIsImF4aXMiLCJjdXJzb3IiLCJvcGFjaXR5Il0sIm1hcHBpbmdzIjoiOzs7O0FBQUFBLE9BQU9DLFFBQVAsRUFBaUJDLEtBQWpCLENBQXVCLFVBQVNDLENBQVQsRUFBVztBQUFBOztBQUNqQ0EsR0FBRSxlQUFGLEVBQW1CQyxRQUFuQjtBQUNDQyxRQUFNLEdBRFA7QUFFQ0MsVUFBUSxNQUZUO0FBR0NDLFdBQVM7QUFIViwyQ0FJUyxNQUpULDRDQUtXLENBTFgsMENBTVMsa0JBQVcsQ0FBRSxDQU50QjtBQVFBLENBVEQiLCJmaWxlIjoic3RhZmYtdXNlci1wb3N0LWxpc3QtYWRtaW4uanMiLCJzb3VyY2VzQ29udGVudCI6WyJqUXVlcnkoZG9jdW1lbnQpLnJlYWR5KGZ1bmN0aW9uKCQpe1xuXHQkKCcubS1zdGFmZi1saXN0Jykuc29ydGFibGUoe1xuXHRcdGF4aXM6ICd5Jyxcblx0XHRjdXJzb3I6ICdtb3ZlJyxcblx0XHRvcGFjaXR5OiAwLjYsXG5cdFx0Y3Vyc29yOiAnbW92ZScsXG5cdFx0ZGlzdGFuY2U6IDUsXG5cdFx0dXBkYXRlOiBmdW5jdGlvbigpIHt9XG5cdH0pO1xufSk7XG4iXX0=
+//# sourceMappingURL=data:application/json;charset=utf8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbInNvcnQuanMiXSwibmFtZXMiOlsialF1ZXJ5IiwiZG9jdW1lbnQiLCJyZWFkeSIsIiQiLCJsZW5ndGgiLCJzb3J0YWJsZSIsImF4aXMiLCJjdXJzb3IiLCJvcGFjaXR5Il0sIm1hcHBpbmdzIjoiOzs7O0FBQUFBLE9BQVFDLFFBQVIsRUFBbUJDLEtBQW5CLENBQTBCLFVBQVVDLENBQVYsRUFBYztBQUN2QyxLQUFLQSxFQUFHLGVBQUgsRUFBcUJDLE1BQXJCLEdBQThCLENBQW5DLEVBQXVDO0FBQUE7O0FBQ3RDRCxJQUFHLGVBQUgsRUFBcUJFLFFBQXJCO0FBQ0NDLFNBQU0sR0FEUDtBQUVDQyxXQUFRLE1BRlQ7QUFHQ0MsWUFBUztBQUhWLDRDQUlTLE1BSlQsNENBS1csQ0FMWCwwQ0FNUyxrQkFBVyxDQUFFLENBTnRCO0FBUUE7QUFDRCxDQVhEIiwiZmlsZSI6InN0YWZmLXVzZXItcG9zdC1saXN0LWFkbWluLmpzIiwic291cmNlc0NvbnRlbnQiOlsialF1ZXJ5KCBkb2N1bWVudCApLnJlYWR5KCBmdW5jdGlvbiggJCApIHtcblx0aWYgKCAkKCAnLm0tc3RhZmYtbGlzdCcgKS5sZW5ndGggPiAwICkge1xuXHRcdCQoICcubS1zdGFmZi1saXN0JyApLnNvcnRhYmxlKHtcblx0XHRcdGF4aXM6ICd5Jyxcblx0XHRcdGN1cnNvcjogJ21vdmUnLFxuXHRcdFx0b3BhY2l0eTogMC42LFxuXHRcdFx0Y3Vyc29yOiAnbW92ZScsXG5cdFx0XHRkaXN0YW5jZTogNSxcblx0XHRcdHVwZGF0ZTogZnVuY3Rpb24oKSB7fVxuXHRcdH0pO1xuXHR9XG59KTtcbiJdfQ==
